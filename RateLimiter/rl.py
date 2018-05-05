049fc12ebf497c95cf2a795703d65613b1d54056
David <youchen.du@gmail.com>
2018-05-05 17:23:44 +0800

    [update] more flexiable for RateLimiter

--- RateLimiter/rl.py
+++ RateLimiter/rl.py
@@ -5,37 +5,44 @@ from time import time as now
 
 class RateLimiter(object):
     bucket = {}
-    def __init__(self, rate=5):
-        self.rate = rate
 
-    def access(self, host, *args, **kwargs):
-        if self.can_access(host):
+    def __init__(self, qps=5):
+        self.qps = qps
+
+    def access(self, host, token=1, *args, **kwargs):
+        if self.can_access(host, token):
             return True
         return False
 
-    def can_access(self, host):
-        host_data = self.bucket.setdefault(host, [now(), 1])
+    def can_access(self, host, token):
+        host_data = self.bucket.setdefault(host, [now(), 0])
         t = now()
-        more_tokens = (t - host_data[0]) / (1 / self.rate)
-        if more_tokens > 0:
-            host_data[1] = min(more_tokens + host_data[1], self.rate)
+        more_tokens = (t - host_data[0]) / (1 / self.qps)
+        if more_tokens >= 1:
+            host_data[1] = min(more_tokens + host_data[1], token)
             host_data[0] = t
-        if host_data[1] > 1:
-            host_data[1] -= 1
+        if host_data[1] >= token:
+            host_data[1] -= token
             return True
         else:
             return False
 
 
 def main():
-    rl = RateLimiter(100)
+    rate = 100
+    rl = RateLimiter(rate)
     start = now()
     cnt = 0
+    max_qps = 0
     while True:
-        if rl.access('localhost'):
+        if rl.access('localhost', 200):
             cnt += 1
+        t = now()
+        qps = cnt / (t - start)
+        max_qps = max(qps, max_qps)
         sys.stdout.flush()
-        sys.stdout.write('{:10d}\r'.format(int(cnt/(now() - start))))
+        sys.stdout.write('QPS:{:010.2f} MAX_QPS:{:010.2f}\r'.format(
+            qps, max_qps))
 
 
 if __name__ == '__main__':
